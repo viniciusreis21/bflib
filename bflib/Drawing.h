8301304c385ebb38e25b86c9b33df964518d6c41
viniciusreis21 <viniciusreis@DESKTOP-OQDBBFJ.localdomain>
2021-05-20 20:29:45 -0300

    Feat: add Drawing class

---
+++ bflib/Drawing.h
@@ -0,0 +1,28 @@
+#ifndef DRAWING_H
+#define DRAWING_H
+
+#include <iostream>
+#include <string>	
+
+
+using namespace std;
+
+/** This is a Drawing class for simulation and tests purpose.
+* The main objective of this class is to generate all the drawing functions for the purposes of simulating our model.
+* 
+*/ 
+
+class Drawing
+{
+    public:
+        Drawing();
+        /** Constructor for Drawing class
+         */ 
+        ~Drawing();
+        /** Destructor for Drawing class
+         */
+
+};
+
+
+#endif
