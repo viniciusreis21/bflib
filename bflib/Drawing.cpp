e262251f3b89ed084d060f73ad148da594b4676e
viniciusreis21 <viniciusreis@DESKTOP-OQDBBFJ.localdomain>
2021-06-11 08:51:27 -0300

    Resolve bug merge

--- bflib/Drawing.cpp
+++ bflib/Drawing.cpp
@@ -11,7 +11,6 @@ Drawing::~Drawing()
 void Drawing::function_test()
 {
     
-<<<<<<< HEAD
 }
 void Drawing::drawParticles(cv::Mat& image, const vector< Robot::State >& PS, const cv::Scalar& color)
 {
@@ -19,6 +18,4 @@ void Drawing::drawParticles(cv::Mat& image, const vector< Robot::State >& PS, co
     {
         cv::circle(image, cv::Point(10 + 100 * PS[i][0], 10 + 100 * PS[i][1]), 2, color, CV_FILLED);
     }
-=======
->>>>>>> e3e59ffb33b1fa682a6526d03192f07f24fea609
 }
