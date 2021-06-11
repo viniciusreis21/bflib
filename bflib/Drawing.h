8e4e73c3088402e16f1b714570b82d6862a38136
Samuel Abreu <samuelapp@outlook.com>
2021-06-11 10:43:36 -0300

    feat: add teste123

--- bflib/Drawing.h
+++ bflib/Drawing.h
@@ -2,7 +2,7 @@
 #define DRAWING_H
 
 #include <iostream>
-#include <string>	
+#include <string>
 #include <vector>
 #include <cmath>
 #include "BFLIB/PF.hpp"
@@ -11,22 +11,26 @@ using namespace std;
 
 /** This is a Drawing class for simulation and tests purpose.
 * The main objective of this class is to generate all the drawing functions for the purposes of simulating our model.
-* 
-*/ 
+*
+*/
 
 class Drawing
 {
     public:
         Drawing();
         /** Constructor for Drawing class
-         */ 
+         */
+        void teste1();
+
         ~Drawing();
         /** Destructor for Drawing class
          */
-
+        void teste2();
         void drawParticles(cv::Mat& image, const vector< Robot::State >& PS, const cv::Scalar& color)
         /** Method that creates particle simulations
-         */ 
+         */
+        void teste33333333();
+
 
 };
 
